31e4235321202ead8db95e55d7ef8f301b2bc0be
Alexandr Popov <He1ton@yandex.ru>
2023-03-06 18:08:06 +0300

    ss

--- Distance.cs
+++ Distance.cs
@@ -4,33 +4,36 @@ using UnityEngine;
 
 public class Distance : MonoBehaviour
 {
-    [SerializeField] private List<Transform> _allPoints;
-    [SerializeField] private Transform _passDistance;
+    [SerializeField] private List<Transform> _allPointList;
     [SerializeField] private float _speed;
-
-    private bool _moving = true;
 
     private Vector3 _target;
     private int _index = 0;
+    private float _distance;
+    private bool _moving;
 
     private void Update()
     {
         transform.Rotate(0, 0, 1);
         transform.LookAt(_target);
-        _target = _passDistance.position;
+        _target = _allPointList[_index].transform.position;
 
-        if (_allPoints[_index].position==_passDistance.position)
+        if (_moving)
         {
-            if (_moving)
+            transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        }
+
+        _distance = Vector3.Distance(transform.position, _target);
+
+        if (_distance <= 0.05f)
+        {
+            if (_allPointList[_index])
             {
-                transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
-                transform.LookAt(_allPoints[_index].position);
+                if (_allPointList[_index].position == _target)
+                {
+                    _target = _allPointList[_index].position;
+                }
             }
         }
-        else
-        {
-            _index++;
-        }
-
     }
 }
